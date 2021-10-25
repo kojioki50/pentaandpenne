0209cd6ca3182bca791a00c8af7e469352523e43
kojioki50 <kojioki50@gmail.com>
2021-10-25 23:20:58 +0900

    top作成

--- js/main.js
+++ js/main.js
@@ -2,10 +2,24 @@ $(function () {
 
   $(".js-menu").click(function () {
     $(".sidebar-container").toggleClass('active');
+    $(".sidebar-mask").toggleClass('active');
 
   });
   $(".close-icon").click(function () {
     $(".sidebar-container").toggleClass('active');
+    $(".sidebar-mask").toggleClass('active');
 
   });
+
+  $('.title').on('click', function () {
+    var findElm = $(this).next(".box");
+    $(findElm).slideToggle();
+    if ($(this).hasClass('close')) {
+      $(this).removeClass('close');
+    } else {
+      $(this).addClass('close');
+    }
+  
+  });
+
 });
